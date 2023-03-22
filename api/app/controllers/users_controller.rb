32db35463d63eb4be2e58874247ffd3e546c73e6
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-03-22 14:09:48 +0300

    SignUp endpoint

---
+++ api/app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+    def register 
+        user = User.create(user_params)
+        if user.valid? 
+            app_response(message: "Registration was successfull", status: :created, data: user )
+        else 
+            app_response(message: "Something went wrong during registration", status: :unprocessable_entity, data: user.errors )
+        end
+    end
+
+    private 
+
+    def user_params 
+        params.permit(:username, :email, :password)
+    end
+end
---
+++ api/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  #user 
+  post '/users', to: 'users#register'
+
+end
